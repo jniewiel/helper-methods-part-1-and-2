8a2e47c788f551c0012df4f60f16339a10fae782
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-26 17:27:44 +0000

    refactored movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,91 +1,57 @@
 class MoviesController < ApplicationController
   def new
-    @the_movie = Movie.new
+    @movie = Movie.new
   end
 
   def index
-    # matching_movies = Movie.all
-
-    # @list_of_movies = matching_movies.order(created_at: :desc)
-
-    # respond_to do |format|
-    # --format.json do
-    # --render json: @list_of_movies
-    # end
-
-    # format.html do
-    # --render "movies/index"
-    # end
-    # end
-
     @movies = Movie.order(created_at: :desc)
 
     respond_to do |format|
-      format.json do
-        render json: @movies
-      end
+      format.json { render json: @movies }
 
       format.html
     end
   end
 
   def show
-    # the_id = params.fetch(:id)
-    # matching_movies = Movie.where(id: the_id)
-    # @the_movie = matching_movies.first
-    # render "movies/show"
-
     @movie = Movie.find(params.fetch(:id))
   end
 
   def create
-    movie_attributes = params.require(:movie).permit(:title, :description)
-    @movie = Movie.new(movie_attributes)
-    # @the_movie = Movie.new
-    # @the_movie.title = params.fetch("query_title")
-    # @the_movie.description = params.fetch("query_description")
-    # @the_movie.title = params.fetch(:movie).fetch(:title)
-    # @the_movie.description = params.fetch(:movie).fetch(:description)
-    
-    if @the_movie.valid?
-      @the_movie.save
+    movie_params = params.require(:movie).permit(:title, :description)
+
+    @movie = Movie.new(movie_params)
+
+    if @movie.valid?
+      @movie.save
+
       redirect_to movies_url, notice: "Movie created successfully."
     else
-      render "movies/new"
+      render "new"
     end
   end
 
   def edit
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where(id: the_id)
-
-    @the_movie = matching_movies.first
-
-    render "movies/edit"
+    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    the_id = params.fetch(:id)
-    the_movie = Movie.where(id: the_id).first
+    @movie = Movie.find(params.fetch(:id))
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
+    movie_params = params.require(:movie).permit(:title, :description)
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to movies_url, notice: "Movie updated successfully."
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie updated successfully."
     else
-      redirect_to movies_url, notice: "Movie failed to update successfully."
+      render "edit"
     end
   end
 
   def destroy
-    the_id = params.fetch(:id)
-    the_movie = Movie.where(id: the_id).first
+    @movie = Movie.find(params.fetch(:id))
 
-    the_movie.destroy
+    @movie.destroy
 
-    redirect_to movies, notice: "Movie deleted successfully."
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
