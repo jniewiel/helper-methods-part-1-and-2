a8241d0af5d265c7bddf61420bcb476caf040457
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-26 16:47:56 +0000

    refactored movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,36 +4,47 @@ class MoviesController < ApplicationController
   end
 
   def index
-    matching_movies = Movie.all
+    # matching_movies = Movie.all
 
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    # @list_of_movies = matching_movies.order(created_at: :desc)
+
+    # respond_to do |format|
+    # --format.json do
+    # --render json: @list_of_movies
+    # end
+
+    # format.html do
+    # --render "movies/index"
+    # end
+    # end
+
+    @movies = Movie.order(created_at: :desc)
 
     respond_to do |format|
       format.json do
-        render json: @list_of_movies
+        render json: @movies
       end
 
-      format.html do
-        render "movies/index"
-      end
+      format.html
     end
   end
 
   def show
-    the_id = params.fetch(:id)
+    # the_id = params.fetch(:id)
+    # matching_movies = Movie.where(id: the_id)
+    # @the_movie = matching_movies.first
+    # render "movies/show"
 
-    matching_movies = Movie.where({ :id => the_id })
-
-    @the_movie = matching_movies.first
-
-    render "movies/show"
+    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
     @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
-
+    # @the_movie.title = params.fetch("query_title")
+    # @the_movie.description = params.fetch("query_description")
+    @the_movie.title = params.fetch(:title)
+    @the_movie.description = params.fetch(:description)
+    
     if @the_movie.valid?
       @the_movie.save
       redirect_to movies_url, notice: "Movie created successfully."
@@ -45,7 +56,7 @@ class MoviesController < ApplicationController
   def edit
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where(id: the_id)
 
     @the_movie = matching_movies.first
 
@@ -54,7 +65,7 @@ class MoviesController < ApplicationController
 
   def update
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where(id: the_id).first
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
@@ -69,7 +80,7 @@ class MoviesController < ApplicationController
 
   def destroy
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where(id: the_id).first
 
     the_movie.destroy
 
